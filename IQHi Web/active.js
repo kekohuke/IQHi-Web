329cb36fa43ee9da57d90692fefdd6da20c5bd57
kekohuke <43626569+kekohuke@users.noreply.github.com>
2019-07-11 23:48:42 -0700

    contact

--- IQHi Web/active.js
+++ IQHi Web/active.js
@@ -1,3 +1,31 @@
+
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+	var params = myform.serializeArray().reduce(function(obj, item) {
+     obj[item.name] = item.value;
+     return obj;
+  }, {});
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+
+  var template_id = "template_I0uPlCfB";
+  myform.find("button").text("Sending...");
+  emailjs.send(service_id, template_id, params)
+  	.then(function(){ 
+       alert("Sent!");
+       myform.find("button").text("Send");
+     }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    });
+
+  return false;
+});
+
+
 (function ($) {
     'use strict';
     var $window = $(window);
@@ -12,3 +40,4 @@
     });
 
 })(jQuery);
+
